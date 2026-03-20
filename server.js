5afc96b4080d037f8f3f39a41dd3e8b4504dc45e
nandinireddy3737 <nandinireddy3737@gmail.com>
2026-03-20 17:30:41 +0530

    fix render routing

--- server.js
+++ server.js
@@ -1,47 +1,19 @@
 const express = require("express");
-const fs = require("fs");
+const path = require("path");
+
 const app = express();
 
-app.use(express.json());
-app.use(express.static("public"));
+// allow frontend files
+app.use(express.static(__dirname));
 
-// 🧾 SAVE ORDER IN FILE
-app.post("/order", (req, res) => {
-  const newOrder = req.body;
-
-  fs.readFile("orders.json", (err, data) => {
-    let orders = [];
-
-    if (!err && data.length > 0) {
-      orders = JSON.parse(data);
-    }
-
-    orders.push(newOrder);
-
-    fs.writeFile("orders.json", JSON.stringify(orders, null, 2), (err) => {
-      if (err) {
-        return res.send("Error saving order");
-      }
-
-      res.send("Order saved successfully");
-    });
-  });
+// HOME PAGE
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// 📦 VIEW ALL ORDERS (ADMIN)
-app.get("/orders", (req, res) => {
-  fs.readFile("orders.json", (err, data) => {
-    if (err) {
-      return res.send([]);
-    }
-
-    res.json(JSON.parse(data));
-  });
-});
+// PORT for Render
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+    console.log("Server running on port " + PORT);
 });
-
- 
